c63e0acf8f98943ab69d3d3f3f132d833e36ee18
Vasi-18 <vasanthkumar1822000@gmail.com>
2023-08-29 21:07:47 +0530

    library

---
+++ src/pages/Homepage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Homepage = () => {
+  return (
+  <div className="row">
+    <div className="col text-center p-5 ">
+    <h2 className="fw-bold heading h1">Library Management System</h2>
+    </div>    
+  </div>
+  );
+};
+
+export default Homepage;
